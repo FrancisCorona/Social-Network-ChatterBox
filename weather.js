f3d31c22c279cf7923f2d734a87c944246ca1173
Francis Corona <thefranciscorona@gmail.com>
2024-05-11 17:43:53 -0400

    Commit. Implemented weather API and print temp, humidity, wind speed, short forecast, and long forecast.

--- weather.js
+++ weather.js
@@ -1,4 +1,27 @@
-const url = 'https://v2.jokeapi.dev/joke/Any'
+const url = 'https://api.weather.gov/gridpoints/TOP/32,81/forecast' // Weather Forecast API URL
+const request = fetch(url) // Fetching url and making a promise 
+                .then(r => r.json()) // Parsing JSON response
+
+                .then(json => {
+                        const periods = json.properties.periods; // Accessing the periods array
+                        for (let i = 0; i < periods.length; i++){ // For loop to iterate over each period
+                                const period = periods[i]; // Defines new const period for each iteration
+
+                console.log("Current Weather");
+                console.log(" ");
+                console.log("Temperature: " + period.temperature); // Print Temperature
+                console.log("Humidity: " + period.relativeHumidity.value); // Print Humidity
+                console.log("Wind Speed: " + period.windSpeed); // Print Wind Speed
+                console.log(period.shortForecast); // Print short forecast
+                console.log(period.detailedForecast); // Print Long forecast
+                console.log(" ");
+                console.log(" ");
+                        }
+                });
+
+
+/*
+const url = 'https://v2.jokeapi.dev/joke/Any' 
 const request = fetch(url)
                 .then(r => r.json())
                 .then(json => {
@@ -6,3 +29,4 @@ const request = fetch(url)
                         setTimeout(x => console.log(json.delivery), 1000);
                 } );
 console.log(request);
+*/
