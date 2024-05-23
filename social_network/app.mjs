1edcd55a5d42070f507cb61a9a1e5361c043822a
ianstewart <ianastewart1021@gmail.com>
2024-05-22 23:04:22 -0400

    created app.mjs

---
+++ social_network/app.mjs
@@ -0,0 +1,9 @@
+// Create a User Schema and Model.
+// Create a Post Schema and Model. A Post should link to the User that created it; so it must have a user field of type mongoose.Schema.Types.ObjectId, and a ref of 'User'. You can read more about linking here.
+// Install Passport and the Passport Local Strategy.
+// Install Express Session middleware and setup to use your database as the session store.
+// Add your Passport serialize and deserialize functions.
+// Add a function that will be used for authentication to protect routes that need it.
+// Add a GET and POST route to a registration form.
+// Add a GET and POST route to a login form.
+// Add a GET and POST route for new posts. On the GET page, display all the previous posts added by the current user.
